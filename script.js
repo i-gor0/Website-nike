ccbe5ff672f626d6db9afe280b9e2409a2c4cba2
igor <igor.antuness28h@gmail.com>
2024-07-08 03:43:09 -0300

    removi e alterei algumas coisas
    adicionei para mudar a cor do texto para preto quando o backtound for branco no javascript

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function changeBackgroundColor(color) {
     const containerTenis = document.querySelector('.conteinertenis');
+    const button = document.querySelector('button');
     containerTenis.style.backgroundColor = color;
-    containerTenis.classList.add('gradient-animation');
-    setTimeout(() => {
-        containerTenis.classList.remove('gradient-animation');
-    }, 500);
+    if(color=='white'){
+        containerTenis.style.color = 'black';
+        containerTenis.style.boxShadow = '1px -1px 10px rgba(0, 0, 0, 0.418)';
+    } else {
+        containerTenis.style.color = 'white';
+        containerTenis.style.boxShadow = '';
+    }
 }
