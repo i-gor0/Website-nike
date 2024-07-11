d6626a0d3c83240983d0347e0b276dfeee3712ca
igor <igor.antuness28h@gmail.com>
2024-07-11 03:36:33 -0300

    melhor organização e entre outras adiçoes
    melhorei as features de mudar as cores dos tenis no javascript, adicionei mais tenis, melhorei a interface, coloquei imagens melhores e com tamanhos definidos, renomeei os arquivos conforme o nescessário

--- script.js
+++ script.js
@@ -1,56 +1,59 @@
-function changeBackgroundColor(color) {
-    const conteinerimg = document.querySelector('.conteinerimg');
-    const btnpreto = document.querySelector('#preto');
-    const btnazul = document.querySelector('#azul');
-    const btnverde = document.querySelector('#verde');
-    const btnbranco = document.querySelector('#branco');
-    const img = document.querySelector('#jordan');
-    const containerTenis = document.querySelector('.conteinertenis');
-    containerTenis.style.backgroundColor = color;
-   
-    switch (color) {
-        case 'black':
-            containerTenis.style.color = 'white';
-            conteinerimg.style.width = '150px';
-            conteinerimg.style.height = '150px';
-            img.setAttribute("src", "./assets/airjordanpreto.png");
-            btnpreto.style.border = '2px solid black';
-            btnpreto.style.boxShadow = '0px 0px 3px rgb(255, 255, 255)';
-            btnbranco.style.border = '2px solid white';
-            btnazul.style.border = '2px solid white';
-            btnverde.style.border = '2px solid white';
-            break;
-        case 'blue':
-            conteinerimg.style.width = '150px';
-            conteinerimg.style.height = '150px';
-            containerTenis.style.boxShadow = '1px -1px 10px rgba(0, 0, 0, 0.418)';
-            img.setAttribute("src", "./assets/airjordanazul.png");
-            btnazul.style.border = '2px solid black';
-            btnpreto.style.border = '2px solid white';
-            btnbranco.style.border = '2px solid white';
-            btnverde.style.border = '2px solid white';
-            break;
-        case 'green':
-            img.setAttribute("src", "./assets/airjordanverde.png");
-            btnverde.style.border = '2px solid black';
-            btnbranco.style.border = '2px solid white';
-            btnazul.style.border = '2px solid white';
-            btnpreto.style.border = '2px solid white';
-            break;
-        case 'white':
-            containerTenis.style.color = 'black';
-            conteinerimg.style.width = '150px';
-            conteinerimg.style.height = '150px';
-            containerTenis.style.boxShadow = '1px -1px 10px rgba(0, 0, 0, 0.418)';
-            img.setAttribute("src", "./assets/airjordan.png");
-            btnbranco.style.border = '2px solid black';
-            btnazul.style.border = '2px solid white';
-            btnpreto.style.border = '2px solid white';
-            btnverde.style.border = '2px solid white';
-            break;
-        default:
-            containerTenis.style.color = 'white';
-            break;
+function changeBackgroundColor(button, color) {
+    // Definindo a cor azul corretamente
+    const blue = '#0A63F0';
+    if (color === 'blue') {
+        color = blue;
     }
-    
+
+    const containerTenis = button.closest('.conteinertenis');
+    const tenisName = containerTenis.querySelector('h1').textContent;
+
+    containerTenis.style.backgroundColor = color;
+
+    if (containerTenis.style.backgroundColor === 'black') {
+        containerTenis.style.color = 'white';
+    } else {
+        containerTenis.style.color = 'black';
+    }
+    changeImg(containerTenis, color, tenisName);
+}
+
+function changeImg(containerTenis, color, tenisName) {
+    const img = containerTenis.querySelector('#tenis');
+    if (tenisName === 'Air Jordan') {
+        switch (color) {
+            case 'black':
+                img.setAttribute("src", "./assets/airjordanpreto.png");
+                break;
+            case '#0A63F0':
+                img.setAttribute("src", "./assets/airjordanazul.png");
+                break;
+            case 'green':
+                img.setAttribute("src", "./assets/airjordanverde.png");
+                break;
+            default:
+                img.setAttribute("src", "./assets/airjordan.png");
+                break;
+        }
+    }
+    if (tenisName === 'Air Max') {
+        switch (color) {
+            case 'black':
+                img.setAttribute("src", "./assets/AirMaxPreto.png");
+                break;
+            default:
+                img.setAttribute("src", "./assets/AirMax.png");
+                break;
+        }
+    }
+    if (tenisName === 'Tn') {
+        switch (color) {
+            case 'white':
+                img.setAttribute("src", "./assets/tnBranco.png");
+                break;
+            default:
+                img.setAttribute("src", "./assets/tn.png");
+                break;
+        }
+    }
 }
